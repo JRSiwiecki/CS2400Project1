9f5c41134296781d61dadeabdc883ca98abab2cb
Joseph <46443751+TheMusket@users.noreply.github.com>
2021-02-15 13:54:04 -0800

    Add files via upload
    Initial implementation of interfaces and created to-dos.

--- LinkedBag.java
+++ LinkedBag.java
@@ -1,6 +1,89 @@
 package cs2400project1;
 
-public class LinkedBag 
+public class LinkedBag implements BagInterface
 {
+
+    @Override
+    public int getCurrentSize() 
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() 
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean add(Object newEntry) 
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Object remove() 
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object anEntry) 
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() 
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int getFrequencyOf(Object anEntry) 
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object anEntry) 
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BagInterface union(BagInterface bag1) 
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BagInterface intersection(BagInterface bag1) 
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BagInterface difference(BagInterface bag1) 
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -2,5 +2,8 @@ package cs2400project1;
 
 public class LinkedBagTest 
 {
-    
+    public static void main(String[] args)
+    {
+        
+    }
 }
